48cbf919cb911e61e8a397ef9c0f33d4f3925060
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 14:13:43 +0300

    PlayingCat

--- PlayingCat.java
+++ PlayingCat.java
@@ -5,3 +5,10 @@ public class Main {
         System.out.println(PlayingCat.isCatPlaying(false, 35));
     }
 }
+
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        int upperLimit = summer ? 45 : 35;
+        return temperature >= 25 && temperature <= upperLimit;
+    }
+}
