f04cd0b05fcd8c9048a72ab49ab42b9b28a18a2b
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 14:08:29 +0300

    BarkingDog

---
+++ BarkingDog.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(BarkingDog.shouldWakeUp(true, 1));
+        System.out.println(BarkingDog.shouldWakeUp(false, 2));
+        System.out.println(BarkingDog.shouldWakeUp(true, 8));
+        System.out.println(BarkingDog.shouldWakeUp(true, -1));
+    }
+}
+
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if(hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else {
+            if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
+                return true;
+            } else return false;
+        }
+    }
+}
