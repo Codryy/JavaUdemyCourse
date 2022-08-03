8c4ca52521a83cfb4f11a7412e95d13b7101c225
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 13:54:28 +0300

    ConstructorsChallenge

--- ConstructorsChallenge.java
+++ ConstructorsChallenge.java
@@ -11,3 +11,39 @@ public class Main {
         System.out.println("Name for customer1: " + customer3.getName() + ", email: " + customer3.getEmailAdress() + " and credit limit: " + customer3.getCreditLimit());
     }
 }
+
+public class VipCustomer {
+
+    private String name;
+    private String emailAdress;
+    private double creditLimit;
+
+    public VipCustomer() {
+        this("Default", "default@gmail.com", 500.00);
+        System.out.println("Calling constructor with no parameters");
+    }
+
+    public VipCustomer(String name, String emailAdress){
+        this(name, emailAdress, 100);
+        System.out.println("Calling constructor with two parameters");
+    }
+
+    public VipCustomer(String name, String emailAdress, double creditLimit){
+        System.out.println("Calling constructors with three parameters");
+        this.name = name;
+        this.emailAdress = emailAdress;
+        this.creditLimit = creditLimit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+}
