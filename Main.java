fa5dd8e12a28b359b9d9b57c359c737d0c27ee85
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 14:20:49 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = -22;
+        System.out.println("The sum of the digits of number " + number + " is: " + EvenDigitSum.getEvenDigitSum(number));
+    }
+}
