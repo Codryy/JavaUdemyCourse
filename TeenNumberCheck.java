90661dea5dea4d6c4a4c23a3e034526c938762f9
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 14:15:21 +0300

    TeenNumberCheck

--- TeenNumberCheck.java
+++ TeenNumberCheck.java
@@ -5,3 +5,13 @@ public class Main {
         System.out.println(TeenNumberChecker.hasTeen(22, 23, 34));
     }
 }
+
+public class TeenNumberChecker {
+    public static boolean hasTeen(int nr1, int nr2, int nr3){
+        return isTeen(nr1) || isTeen(nr2) || isTeen(nr3);
+    }
+
+    public static boolean isTeen(int nr1){
+        return (nr1 >= 13 && nr1 <= 19);
+    }
+}
