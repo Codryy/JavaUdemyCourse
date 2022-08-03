c048a52b3714ef765a4d82e1b2e0e24a43e2c38b
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 14:10:57 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LeapYear.isYearLeap(-1600));
+        System.out.println(LeapYear.isYearLeap(1600));
+        System.out.println(LeapYear.isYearLeap(2017));
+        System.out.println(LeapYear.isYearLeap(2000));
+    }
+}
