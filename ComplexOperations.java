0170c2baa6a287ef4d5be0abe301405f9a9ca422
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 14:35:33 +0300

    ComplexOperations

---
+++ ComplexOperations.java
@@ -0,0 +1,58 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+    }
+}
+
+public class ComplexNumber {
+
+    private double real, imaginary;
+
+    public ComplexNumber(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber){
+//        this.real += complexNumber.getReal();
+//        this.imaginary += complexNumber.getImaginary();
+        add(complexNumber.getReal(), complexNumber.getImaginary());
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber){
+//        this.real -= complexNumber.getReal();
+//        this.imaginary -= complexNumber.getImaginary();
+        subtract(complexNumber.getReal(), complexNumber.getImaginary());
+    }
+
+}
