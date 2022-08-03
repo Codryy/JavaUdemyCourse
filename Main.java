6c8a23bc8475f7ca4ad2beb450890a773f80e808
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 14:23:42 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int first = 1010;
+        int second = 10;
+        System.out.println("Greatest Common Divisor between " + first + " and " + second + " is: " + GreatestCommonDivisor.getGreatestCommonDivisor(first, second));
+    }
+}
