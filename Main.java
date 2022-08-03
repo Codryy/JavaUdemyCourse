6721f988e7546612624751a8b68a8828da14ad09
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 14:19:05 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = -1221;
+        System.out.println("The number " + number + " is palindrome? " + NumberPalindrome.isPalindrome(number) );
+    }
+}
