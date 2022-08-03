298027dc0323814ed3a6ff5f579b2b6caae5f06c
Codry <100705769+Codryy@users.noreply.github.com>
2022-08-03 14:25:17 +0300

    PerfectNumber

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int number = 33550336;
-        System.out.println(PerfectNumber.isPerfectNumber(number));
-    }
-}
---
+++ PerfectNumber.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 33550336;
+        System.out.println(PerfectNumber.isPerfectNumber(number));
+    }
+}
+
+public class PerfectNumber {
+
+    public static boolean isPerfectNumber(int number) {
+        if(number < 1) {
+            return false;
+        }
+        int sum=0;
+        for (int i=1; i<= number/2; i++)
+            if(number % i == 0) sum += i;
+        return sum == number;
+    }
+}
